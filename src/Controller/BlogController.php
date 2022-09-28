2406b04aeb74eff964226115814d7ef9ee627999
rezzouq <mohammed.rezzouq@siway.ma>
2022-09-28 10:02:17 +0100

    ci(creating smart forms):

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,6 +41,36 @@ class BlogController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create(Request $request, EntityManagerInterface $manager)
+    {
+        // dump($request);
+        // die;
+        // if ($request->request->count() > 0) {
+        //     $article = new Article();
+        //     $article->setTitle($request->request->get('title'))
+        //         ->setContent($request->request->get('content'))
+        //         ->setImage($request->request->get('image'))
+        //         ->setCreatedAt(new \DateTime());
+
+        //     $manager->persist($article);
+        //     $manager->flush();
+        //     return $this->redirectToRoute("blog_show", ['id' => $article->getId()]);
+        // }
+
+        //we can do instead pour creer le code html formulaire par symfony
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, ['attr' => ['placeholder' => "Titre de l'article", 'class' => 'form-control']])
+            ->add('content', TextareaType::class, ['attr' => ['placeholder' => "Contenu de l'article", 'class' => 'form-control']])
+            ->add('image', TextType::class, ['attr' => ['placeholder' => "Image de l'article", 'class' => 'form-control']])
+            ->getForm();
+
+        return $this->render('blog/create.html.twig', ['formArticle' => $form->createView()]);
+    }
 
     /**
      * @Route("/blog/{id}", name="blog_show")
